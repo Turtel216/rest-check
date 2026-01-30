4248359a3a523ee857a4e2f988d64c268a9b4ba8
papakonstantinou <papakonstantinou.dm@gmail.com>
2026-01-30 15:24:34 +0200

    Add Executor module

--- app/Main.hs
+++ app/Main.hs
@@ -1,28 +1,35 @@
 module Main where
 
-import qualified Data.Text
+import qualified Data.Text as T
 import Parser (parseConfig)
+import Executor (runRequest)
 import Text.Megaparsec (errorBundlePretty)
 
 -- Sample configuration text
 sampleConfig :: String
 sampleConfig = unlines
   [ "# This is a comment"
-  , "GET https://api.github.com/users/octocat"
+  , "GET https://httpbin.org/get"
   , ""
   , "Header: User-Agent: RestCheck-CLI"
-  , "Header: Accept: application/json"
   , ""
   , "EXPECT Status 200"
   , "EXPECT Latency < 500"
-  , "EXPECT BodyContains \"login\""
   ]
 
 main :: IO ()
 main = do
-  let result = parseConfig (Data.Text.pack sampleConfig)
+  putStrLn "--- 1. Parsing Config ---"
+  let result = parseConfig (T.pack sampleConfig)
+  
   case result of
     Left err -> putStrLn (errorBundlePretty err)
     Right ast -> do
-      putStrLn "--- Parsed Successfully ---"
+      putStrLn "Parsed successfully. AST:"
       print ast
+      
+      putStrLn "\n--- 2. Executing Request ---"
+      actual <- runRequest ast
+      
+      putStrLn "Response received:"
+      print actual
---
+++ src/Executor.hs
@@ -0,0 +1,57 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Executor where
+
+import Parser  (RequestConfig(..), HttpMethod(..))
+import Data.Text(Text, unpack)
+import qualified Data.CaseInsensitive as CI
+import qualified Data.ByteString.Char8 as BS
+import qualified Data.Text.Encoding as TE
+import Network.HTTP.Types.Status (statusCode)
+import Network.HTTP.Simple
+import qualified Data.ByteString.Lazy as LBS
+import Data.Time.Clock (getCurrentTime, nominalDiffTimeToSeconds, diffUTCTime)
+
+data ActualResponse = ActualResponse
+ { actualStatus :: Int
+ , actualBody :: Text
+ , actualLatency :: Int
+ } deriving (Show)
+
+-- Convert AST Method to ByteString format
+methodToBS :: HttpMethod -> BS.ByteString
+methodToBS GET = "GET"
+methodToBS POST = "POST"
+methodToBS PUT = "PUT"
+methodToBS DELETE = "DELETE"
+
+-- Convert parser header to RequestHeaders
+headersToHeaderList :: [(Text, Text)] -> RequestHeaders
+headersToHeaderList = map toHeader
+  where
+    toHeader (k, v) = (CI.mk (TE.encodeUtf8 k), TE.encodeUtf8 v)
+
+runRequest :: RequestConfig -> IO ActualResponse
+runRequest config = do
+  initialRequest <- parseRequest (unpack $ url config)
+
+  let request = setRequestMethod (methodToBS $ method config)
+                $ setRequestHeaders (headersToHeaderList $ headers config)
+                $ initialRequest
+
+  start <- getCurrentTime 
+
+  response <- httpLBS request
+
+  end <- getCurrentTime
+
+  let latencySecond = nominalDiffTimeToSeconds (diffUTCTime end start)
+  let latencyMs = round (latencySecond * 1000)
+
+  let bodyText = TE.decodeUtf8 (LBS.toStrict $ getResponseBody response)
+
+  return ActualResponse
+    { actualStatus = statusCode (getResponseStatus response)
+    , actualBody = bodyText
+    , actualLatency = latencyMs
+    }
